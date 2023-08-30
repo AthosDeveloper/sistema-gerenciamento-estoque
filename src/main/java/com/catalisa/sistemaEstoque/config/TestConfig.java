4ec53ac43a4fe7091718dcc57473a4bf582becd3
AthosDeveloper <athos.silva@zup.com.br>
2023-08-30 14:20:55 -0300

    starting configuration of profile

---
+++ src/main/java/com/catalisa/sistemaEstoque/config/TestConfig.java
@@ -0,0 +1,24 @@
+package com.catalisa.sistemaEstoque.config;
+
+import com.catalisa.sistemaEstoque.model.Product;
+import com.catalisa.sistemaEstoque.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+    @Autowired
+    private ProductRepository repository;
+    @Bean
+    public  void startDB(){
+       Product product1 = new Product(null, "playstation4", "jogue em seu console", new BigDecimal("3.546"), 1);
+   Product product2 = new Product(null, "computador", "um computador de qualidade de intelcore i 9", new BigDecimal("6.546"), 1);
+   repository.saveAll(List.of(product1, product2));
+    }
+}
--- src/main/java/com/catalisa/sistemaEstoque/model/Product.java
+++ src/main/java/com/catalisa/sistemaEstoque/model/Product.java
@@ -1,11 +1,19 @@
 package com.catalisa.sistemaEstoque.model;
 
+import lombok.*;
+
 import javax.persistence.*;
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Table(name = "tb_products")
+@Getter
+@Setter
+@EqualsAndHashCode(exclude = {"description", "quantity"})
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-public class Product {
+public class Product implements Serializable {
    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
