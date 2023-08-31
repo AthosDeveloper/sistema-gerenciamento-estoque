bf36667ef975d5dfba3aff82fd36c4f7b806fa61
AthosDeveloper <athos.silva@zup.com.br>
2023-08-31 13:45:39 -0300

    CaseTest of findAll was finished

--- src/test/java/com/catalisa/sistemaEstoque/servicesImpl/ProductServiceImplTest.java
+++ src/test/java/com/catalisa/sistemaEstoque/servicesImpl/ProductServiceImplTest.java
@@ -15,6 +15,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
@@ -25,7 +26,7 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 public class ProductServiceImplTest {
     private static  final Long ID = 1L;
-private static  final Long INDEX = 0L;
+private static  final Integer INDEX = 0;
 private static final String NAME = "computador";
 private  static  final  String DESCRIPTION = "computador de mesa";
 private  static  final BigDecimal PRICE = new BigDecimal("4893.3512");
@@ -83,6 +84,28 @@ assertEquals(ObjectNotFoundException.class, ex.getClass());
 //garanta que a mensagem passada na classe objectNotFound seja a mesma da mensagem capturada na exceção
     assertEquals(PRODUTO_NAO_ENCONTRADO, ex.getMessage());
 }
+
+}
+@Test
+public void returnAListOfProducts_whenFindAll(){
+    //quando o repositório buscar todos os produtos por id, me retorna uma lista de produtos
+
+    when(repository.findAll())
+        .thenReturn(List.of(product));
+List<Product> responce = service.findAll();
+//me garanta que a lista de produtos existe
+assertNotNull(responce);
+//garanta que a quantidade de produtos seja igual a quantidade passada no service
+assertEquals(1, responce.size());
+//garanta que a quantidade de classes seja equivalente a quantidade de classes recebidas no service e que sejam a mesma classe
+assertEquals(Product.class, responce.get(INDEX).getClass());
+//garanta que a quantidade de id seja o mesmo
+assertEquals(ID, responce.get(INDEX).getId());
+//o mesmo teste para todos os atributos...
+    assertEquals(NAME, responce.get(INDEX).getName());
+    assertEquals(DESCRIPTION, responce.get(INDEX).getDescription());
+    assertEquals(PRICE, responce.get(INDEX).getPrice());
+assertEquals(QUANTITY, responce.get(INDEX).getQuantity());
 }
 private  void startProduct(){
 product = new Product(ID, NAME, DESCRIPTION, PRICE, QUANTITY);
